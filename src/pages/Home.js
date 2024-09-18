d2b4490a7fc92ba6890efb1e9e3e6939d7c0935f
borodkoleg <borodkoleg@gmail.com>
2024-09-18 21:56:38 +0300

    createBrowserRouter simple example

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <>My Home page</>
+}
+
+export default HomePage;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+  return <>Products page</>
+}
+
+export default ProductsPage;
